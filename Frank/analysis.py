08e0f38d962b0484d440aa5e01a45cf5ac54702a
jadencordeiro <jaden.cabralcordeiro@mail.utoronto.ca>
2025-02-07 23:19:17 -0500

    Frank

--- Frank/analysis.py
+++ Frank/analysis.py
@@ -5,9 +5,9 @@
 import numpy as np
 import pandas as pd
 from scipy.optimize import curve_fit
-from scipy.constants import c, mu_0, epsilon_0
 from scipy.stats import chi2
 import matplotlib.pyplot as plt
+
 
 def access_data(file_name):
     data = pd.read_csv(file_name, comment='#')
@@ -48,10 +48,37 @@ def plot_residuals(x, y, xerr, yerr, model_name, x_axis, y_axis):
     plt.show()
     return None
 
-data = access_data('data/5V_2.5V.csv')
+def myGauss(x, A, mean, width, base):
+    return A*np.exp(-(x-mean)**2/(2*width**2)) + base
 
-current, voltage = data[:,5], data[:,-3]
+def parabolic(x, a, b, c, d):
+    return a*(x-d)**2+b*x+c
 
-plot_data2(current, voltage, np.zeros(len(current)), (10**6)*lc_model(lc_unit, *popt), 'LC Slope', 'LC Units', 'Delay Time (microseconds)')
-plot_residuals(lc_unit, residuals, None, delta_unc, 'LC Slope', 'LC units', 'Residuals (microseconds)')
+def sinusoid(x, a, phi, d, c):
+    return a*np.sin(c*x-phi) + d
+
+
+data = access_data('data/6V_3V.csv')
+current, voltage = data[:,4], data[:,6]
+
+current_unc = current * 0.001 + (50*10**(-12))
+
+voltage_unc_1 = voltage[:48] * 0.00015 + (225*10**(-6))
+voltage_unc_2 = voltage[48:477] * 0.0002 + (350*10**(-6))
+voltage_unc_3 = voltage[477:] * 0.00015 + (5*10**(-3))
+voltage_unc = np.hstack((voltage_unc_1, voltage_unc_2, voltage_unc_3))
+
+lb, ub = (420, 550)
+
+model = sinusoid
+p0 = [0.0001, 17, 1]
+popt, pstd = model_processing(model, voltage[lb:ub], current[lb:ub], current_unc[lb:ub], p0)
+chi, residuals, chi_prob = red_chi_squared(voltage[lb:ub], current[lb:ub], model, popt , current_unc[lb:ub])
+
+plot_data2(voltage[lb:ub], current[lb:ub], current_unc[lb:ub], model(voltage[lb:ub], *popt), 'Gauss', 'Voltage (V)', 'Current (A)')
+plot_residuals(voltage[lb:ub], residuals, voltage_unc[lb:ub], current_unc[lb:ub], 'Gauss', 'Voltage (V)', 'Current (A)')
+
+print(chi_prob, chi)
+print(popt, pstd)
+
 
